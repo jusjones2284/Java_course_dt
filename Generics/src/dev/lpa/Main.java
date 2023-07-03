91c58c3811cfcc8ee1765d41dcb97fee001b8201
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-07-03 19:51:59 -0400

    working with generics

---
+++ Generics/src/dev/lpa/Main.java
@@ -0,0 +1,12 @@
+package dev.lpa;
+
+record BaseballPlayer(String name, String position){
+
+}
+public class Main {
+
+    public static void main(String[] args) { // Press ⌃R or click the green arrow button in the gutter to run the code.
+
+
+    }
+}
