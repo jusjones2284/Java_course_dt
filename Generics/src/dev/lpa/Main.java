a51bcf63d73b8a2c8f416d913132680c58d01034
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-07-04 11:24:36 -0400

    created funcitonality

--- Generics/src/dev/lpa/Main.java
+++ Generics/src/dev/lpa/Main.java
@@ -7,6 +7,21 @@ public class Main {
 
     public static void main(String[] args) { // Press ⌃R or click the green arrow button in the gutter to run the code.
 
+        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros = new BaseballTeam("Houston Astros");
 
+        var harper = new BaseballPlayer("B Harper", "Right Fielder");
+        var marsh = new BaseballPlayer("B Marsh", "Right Fielder");
+        phillies.addTeamMember(harper);
+        phillies.addTeamMember(marsh);
+        phillies.listTeamMembers();
+    }
+
+    public static void scoreResult(BaseballTeam team1, int t1_score,
+                                   BaseballTeam team2, int t2_score){
+
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
     }
 }
