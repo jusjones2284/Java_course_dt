65f786cc367efb424c9221fccb7202c98703bebb
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-07-09 15:18:25 -0400

    review

---
+++ Business/src/ArrayDemo.java
@@ -0,0 +1,10 @@
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        System.out.println(args.length);
+
+
+        int[] nums = {10,20,30,40,50};
+        System.out.println(nums[1]);
+    }
+}
--- Business/src/Person.java
+++ Business/src/Person.java
@@ -1,6 +1,7 @@
 import business.Company;
 public class Person {
     //constant
+    private String middleName = "Mickal";
     private final double PI = 3.14;
     private final String MY_BIG_CONSTANT = "Hi, this is my constant message";
     private String firstName = "Jerry";
@@ -12,7 +13,31 @@ public class Person {
     static private Company company = new Company();
     private NewsAgency agency;
 
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public void saySomething(String message) {
+        System.out.println(message + " this is my message");
+    }
+
+    public char getMiddleInitial(){
+        return middleName.charAt(0);
+    }
+    public int add(int num1, int num2){
+        int total = num1 + num2;
+        return total;
+    }
+
     public static void main(String[] args) {
-        System.out.println(company);
+        Person p1 = new Person();
+        p1.sayHello();
+        p1.saySomething("my name is Justin");
+        System.out.println(p1.getMiddleInitial());
+        System.out.println(p1.add(5,10));
+        char middleInital = p1.getMiddleInitial();
+        System.out.println(middleInital);
+        Person p2 = new Person();
+
     }
 }
