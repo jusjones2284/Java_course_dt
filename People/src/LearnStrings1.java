f2f0213ecd9ebd1ae21f09763c16b9192c03982d
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-07-26 22:54:04 -0400

    working with strings

--- People/src/LearnStrings1.java
+++ People/src/LearnStrings1.java
@@ -34,8 +34,8 @@ public class LearnStrings1 {
 //        String myText = "Four score and seven years ago";
 //        System.out.println(myText.contains("seven"));
 
-        String text1 = "this is my text1";
-        String text2 = "this is my text2";
+//        String text1 = "this is my text1";
+//        String text2 = "this is my text2";
 
 //        System.out.println(text1.concat(" ".concat(text1)));
 //
@@ -85,12 +85,63 @@ public class LearnStrings1 {
 //                .append(myText1)
 //                .append(myText2);
 //        System.out.println(builder.toString());
-        String myText = "apple";
-        String firstPart = myText.substring(0, 1);
-        String lastPart = myText.substring(1);
-        String myNewText = firstPart.toUpperCase() + lastPart;
-        String myNewText1 = myNewText.substring(0,1).toUpperCase().concat(myText.substring(1));
-        System.out.println(myNewText);
+//        String myText = "apple";
+//        String firstPart = myText.substring(0, 1);
+//        String lastPart = myText.substring(1);
+//        String myNewText = firstPart.toUpperCase() + lastPart;
+//        String myNewText1 = myNewText.substring(0,1).toUpperCase().concat(myText.substring(1));
+//        System.out.println(myNewText);
+//        String myNewText3 = new StringBuilder(myText)
+//                .append(myNewText1)
+//                .append(lastPart)
+//                .append(firstPart)
+//                .toString();
+
+        String myText = "four score And B seven 9 years ago";
+        String phoneNumber = "(234) 333-5551".strip();
+        String areaCode = parseAreaCode(phoneNumber);
+        System.out.println(areaCode);
+        String exchange = parseExchange(phoneNumber);
+        System.out.println(exchange);
+        System.out.println(parseLineNumber(phoneNumber));
+        String lineNumber = "";
+
+        String myNewNumber = new StringBuilder(areaCode).append(" ")
+                .append(exchange)
+                .append(parseLineNumber(phoneNumber))
+                .toString();
+        System.out.println(myNewNumber);
+
+
+
+
+
+
+//        System.out.println(myText.indexOf(57) + 23);
+
+    }
+
+    public static String parseAreaCode(String phoneNumber){
+        int openParens = phoneNumber.indexOf("(");
+        int closeParens = phoneNumber.indexOf(")");
+       String areaCode = phoneNumber.substring(openParens + 1, closeParens );
+       return areaCode;
+    }
+
+    public static String parseExchange(String phoneNumber){
+        int spaceIndex = phoneNumber.indexOf(" ");
+        int hyphenIdx = phoneNumber.indexOf("-");
+        String exchanges = phoneNumber.substring(spaceIndex + 1, hyphenIdx );
+        int beginExchange = phoneNumber.indexOf("3", 4);
+        int endingExchange = phoneNumber.lastIndexOf("3");
+        String exchange = phoneNumber.substring(beginExchange, beginExchange + 3);
+        return exchanges;
+    }
+
+    public static String parseLineNumber(String phoneNumber){
+        int hyphenIdx = phoneNumber.indexOf("-");
+
+       return phoneNumber.substring(hyphenIdx );
 
     }
 }
