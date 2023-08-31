0190d96e11bdf3e4dbd2ebfb330a4f505e7d2696
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-08-30 22:38:15 -0400

    java hw

---
+++ Selections/src/Chapter3.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class Chapter3 {
+
+//
+//    if(radius < 0){
+//        System.out.println("Incorrect input");
+//    } else{
+//        double area = radius * radius * 3.14159;
+//        System.out.println("Area is " + area);
+//    }
+
+    public static void main(String[] args){
+//
+//            int number1 = (int) (System.currentTimeMillis() % 10);
+//            int number2 = (int) (System.currentTimeMillis() * 7 % 10);
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("What is " + number1 + " + " + number2 + "? ");
+//        int answer = input.nextInt();
+//
+//        System.out.println(
+//                number1 + " + number2" + " = "+ answer + " is " + (number1 + number2 == answer)
+//        );
+//        int widthOfBox = 5;
+//        int widthOfBook = 4;
+//
+//        Scanner inputs = new Scanner(System.in);
+//
+//        System.out.print("Enter an integer: ");
+//        int number = inputs.nextInt();
+//
+//        if(number % 5 == 0){
+//            System.out.println("hiFive");
+//        }
+//
+//        if(number % 10 == 0){
+//            System.out.println("hi 11");
+//        }
+//        int income = 4001;
+//
+//        if (income > 3000) {
+//            System.out.println("Income is greater than 3000");
+//        }
+//        else if (income > 4000) {
+//            System.out.println("Income is greater than 4000");
+//        }
+//        int i = 19;
+//        int j = 20;
+//
+//        int k = 18;
+//
+//        if (i > k){
+//            System.out.println("i > k but not j");
+//            if(i > j)
+//                System.out.println("i and j are greater than k");
+//        } else
+//            System.out.println(" is less than or equal to k");
+//
+//        long score = 50;
+//
+//        if (score >= 90){
+//            System.out.println("A");
+//        }
+//        int x = 10;
+//        int y = 3;
+//        if(x > 2){
+//            if(y > 2){
+//                int z = x + y;
+//                System.out.println("z is " + z);
+//            } else System.out.println("x is " + x);
+//        }
+        int age = 66;
+        int minors = 0;
+        int adults = 1;
+        int seniors = 2;
+
+
+        if (age < 18){
+            minors +=1;
+            System.out.println(minors);
+        } else if (age <= 64){
+            adults += 1;
+            System.out.println(adults);
+
+        } else if (age >= 65) {
+            adults += 10;
+            seniors += 10;
+            System.out.println(adults + " " + seniors);
+
+        }
+
+    }
+
+
+//
+
+}
