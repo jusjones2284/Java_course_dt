3fb0043dc33daaf02e1e650cbb7e7cd9421fd39d
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-12-31 00:17:24 -0500

    working in java

---
+++ simplerLoop/src/RegexForLoop.java
@@ -0,0 +1,12 @@
+public class RegexForLoop {
+    public static void main(String[] args) {
+       String people =  """
+                Flinstone, Fred, 1/1/1900
+                Rubble, Barney, 2/2/1905
+                Flinstone, Wilma, 3/3/1910
+                Rubble, Betty, 4/4/1915
+                """;
+
+       String regex = "(?<lastName>\\+), \\"
+    }
+}
---
+++ simplerLoop/src/forLoopExample.java
@@ -0,0 +1,13 @@
+public class forLoopExample {
+    public static void main(String[] args) {
+//        for (int i = 0; i <= 10; i++){
+//            System.out.println(i);
+//        }
+
+        String [] fruits = {"apple", "orange", "pear", "plum"};
+
+        for(String fruit: fruits){
+            System.out.println(fruit);
+        }
+    }
+}
